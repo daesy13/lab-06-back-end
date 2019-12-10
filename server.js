55fc0dc9e0923c6cf2684a0777d56792e4f9b13a
Mattäus S. Isenberg <mattaus@gmx.com>
2019-12-10 12:40:30 -0800

    commit with weather functional

--- server.js
+++ server.js
@@ -14,16 +14,35 @@ app.use(express.static('./public'));
 
 function handleLocationRequest(query){
     const geoData = require('./data/geo.json');
-    console.log(geoData);
-    //const locationResult = geoData.results[0];
+    //console.log(geoData);
     const location = new Location(geoData.results[0]);
     return location;
   }
-  
+ // let weatherLocation = [];
+
   function Location(coordinates){
     this.city = coordinates.formatted_Address;
     this.latitude = coordinates.geometry.location.lat;
     this.longitude = coordinates.geometry.location.lng;
+    // weatherLocation.push(this.latitude);
+    // weatherLocation.push(this.longitude);
+  }
+
+  function Weather(location){
+    this.forecast = location.summary;
+    this.time = new Date(location.time * 1000).toDateString();
+  }
+
+function weatherSearch(request, response){
+    const weather = weatherResponse(request.query.data);
+    response.send(weather);
+}
+
+  function weatherResponse(location){
+    let darkSky = require('./data/darksky.json');
+    let weatherArray = [];
+    darkSky.daily.data.forEach(forecast => weatherArray.push(new Weather(forecast)));
+    return weatherArray; 
   }
 
 function responseFunction(request, response){
@@ -33,6 +52,7 @@ function responseFunction(request, response){
 
 
 app.get('/location', responseFunction);
+app.get('/weather', weatherSearch);
 
 
 
